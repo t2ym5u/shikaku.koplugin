d2f2130ec48218b86fda2687a18eb5d966401a15
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 13:41:20 +0200

    chore: bump version to 1.1.3

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "shikaku",
     fullname    = _("Shikaku"),
     description = _("Divide the grid into rectangles"),
-    version     = "1.1.2",
+    version     = "1.1.3",
 }
