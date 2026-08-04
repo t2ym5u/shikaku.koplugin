f57a44bc363d15fb00f5f7e911f2307e2d8b24af
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-04 12:51:38 +0200

    chore: bump version to 1.1.12

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Shikaku"),
     description = _("Divide the grid into rectangles"),
-    version     = "1.1.11",
+    version     = "1.1.12",
 }
